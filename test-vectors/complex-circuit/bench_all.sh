5c1293ffb18b9545f94b63804267337ffd5ee7ae
Ian Klatzco <124045038+ik-work-gh@users.noreply.github.com>
2025-03-28 12:16:26 -0700

    Update bench_all.sh

--- test-vectors/complex-circuit/bench_all.sh
+++ test-vectors/complex-circuit/bench_all.sh
@@ -2,7 +2,9 @@ MIN_NUM_VARIABLES=$1
 MAX_NUM_VARIABLES=$2
 MAX_NUM_CONSTRAINTS=$3
 
-for i in `seq 10 19`; do wget https://hermez.s3-eu-west-1.amazonaws.com/powersOfTau28_hez_final_$i.ptau; done
+echo "Bucket has been deleted"
+exit 1
+# for i in `seq 10 19`; do wget https://hermez.s3-eu-west-1.amazonaws.com/powersOfTau28_hez_final_$i.ptau; done
 
 ./prepare.sh $MIN_NUM_VARIABLES $MAX_NUM_VARIABLES $MAX_NUM_CONSTRAINTS
 
